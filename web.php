53f824fcdaac0e36f1f3e2f6b245107a6d54c8a1
guomin <yushi5344@gmail.com>
2017-06-28 20:07:28 +0800

    p标签

--- web.php
+++ web.php
@@ -3,8 +3,17 @@
 <head>
 <meta charset="utf-8">
 <title>title</title>
+<link rel="stylesheet" href="css/bootstrap.min.css" />
 </head>
 
 <body>
+<!-- bootstrap主要分为三大核心  css  js  fonts -->
+<p>Bootstrap框架</p>
+<p class="lead">Bootstrap框架</p>
+<!-- bootstrap将全局font-size设置为14px,line-height行高设置为1.428  20px，p段落元素等于1/2行高   颜色为#333 
+h1-h6大小  36  30   24   18  14 12-->
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
 </body>
 </html>
